c810db9a4e0e0b9d7d9c215275e2d963ce99179f
isolight <jnbau@gmx.de>
2023-09-06 17:46:21 +0200

    2 Player controls
    2 Player controls
    - For now only one player can dash
    - Only non player has the clash effect that pushes him back

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb2d;
     private Vector2 moveInput;
+    private float dirX, dirY;
 
     public float activeMoveSpeed;
     public float dashSpeed;
@@ -34,16 +35,46 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (canMove)
+        if (name == "Player1")
         {
-            moveInput.x = Input.GetAxisRaw("Horizontal");
-            moveInput.y = Input.GetAxisRaw("Vertical");
+            if (canMove)
+            {
+                dirX = Input.GetAxisRaw("Horizontal") * activeMoveSpeed;
+                dirY = Input.GetAxisRaw("Vertical") * activeMoveSpeed;
+            }
+        }
+        if (name == "Player2" && Input.anyKey)
+        {
+            if (canMove)
+            {
+                if (Input.GetKey(KeyCode.I))
+                {
+                    dirY = activeMoveSpeed;
+                }
+                if (Input.GetKey(KeyCode.K))
+                {
+                    dirY = -activeMoveSpeed;
+                }
+                if (Input.GetKey(KeyCode.J))
+                {
+                    dirX = - activeMoveSpeed;
+                }
+                if (Input.GetKey(KeyCode.L))
+                {
+                    dirX = activeMoveSpeed;
+                }
+            }
+        }
+        else if (name == "Player2" && !Input.anyKey)
+        {
+            dirX = 0f;
+            dirY = 0f;
         }
 
+        moveInput = new Vector2(dirX, dirY);
         moveInput.Normalize();
 
         rb2d.velocity = moveInput * activeMoveSpeed;
-
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -53,6 +84,7 @@ public class PlayerController : MonoBehaviour
                 destroyTrigger.SetActive(true);
                 activeMoveSpeed = dashSpeed;
                 dashCounter = dashLength;
+                rb2d.velocity = new Vector2(dirX, dirY) * 3;
             }
         }
 
