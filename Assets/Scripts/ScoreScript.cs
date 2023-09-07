4a42dabdcdcc72804bddf54ca88ef2516d5ace05
isolight <jnbau@gmx.de>
2023-09-07 20:36:17 +0200

    Win condition, win text

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -2,20 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreScript : MonoBehaviour
 {
     public Text MyscoreText;
     public int ScoreNum;
+    public int winScore;
 
     public DestroyEffect destroyEffect;
+
+    public float slowMotionTimeScale;
+
+    private float startTime;
+    private float startFixedDeltaTime;
+    private float startTimeScale;
+
+    public GameObject fire;
+
+    public GameObject playerOneText;
+    public GameObject playerTwoText;
 
     // Start is called before the first frame update
     void Start()
     {
-        ScoreNum = 0;
+        ScoreNum = -1;
         MyscoreText.text = "Score: " + ScoreNum;
         destroyEffect = GetComponent<DestroyEffect>();
+
+        startTimeScale = Time.timeScale;
+        startFixedDeltaTime = Time.fixedDeltaTime;
+        fire.SetActive(false);
+        playerOneText.SetActive(false);
+        playerTwoText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,5 +45,49 @@ public class ScoreScript : MonoBehaviour
             ScoreNum += 1;
             MyscoreText.text = "Score: " + ScoreNum;
         }
+
+        if (ScoreNum == winScore - 1)
+        {
+            fire.SetActive(true);
+        }
+        
+        if (ScoreNum == winScore)
+        {
+            
+
+            if (name == "Player1")
+            {
+                Invoke("PlayerOneWin", 1f);
+                //StartSlowMotion();
+            }
+            
+            if (name == "Player2")
+            {
+                Invoke("PlayerTwoWin", 1f);
+                //StartSlowMotion();
+            }
+        }
+    }
+    void StartSlowMotion()
+    {
+        Time.timeScale = slowMotionTimeScale;
+        Time.timeScale = startFixedDeltaTime * slowMotionTimeScale;
+    }
+
+    void StopSlowMotion()
+    {
+        Time.timeScale = startTimeScale;
+        Time.fixedDeltaTime = startFixedDeltaTime;
+    }
+    void PlayerOneWin()
+    {
+        StopSlowMotion();
+        playerOneText.SetActive(true);
+    }
+
+    void PlayerTwoWin()
+    {
+        StopSlowMotion();
+        playerTwoText.SetActive(true);
     }
 }
